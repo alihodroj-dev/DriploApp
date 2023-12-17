e623aa584d793dfa48e7f388e9e03408236b153b
Ali Hodroj <aloshito2004@gmail.com>
2023-12-17 22:01:04 +0200

    Initial Commit

---
+++ Driplo/DriploApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriploApp.swift
+//  Driplo
+//
+//  Created by Ali Hodroj on 17/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct DriploApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
