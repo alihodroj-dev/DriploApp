60fcfd9ce960cca71d85741ef41ee9b758f7e3d9
Ali Hodroj <aloshito2004@gmail.com>
2023-12-18 02:45:15 +0200

    Commit 3 -inital HomeView TopBar Design

--- Driplo/Views/HomeView.swift
+++ Driplo/Views/HomeView.swift
@@ -8,12 +8,66 @@
 import SwiftUI
 
 struct HomeView: View {
+    
     var body: some View {
-        Text(UserDefaults.standard.string(forKey: "KLEB") ?? "")
-        Text(UserDefaults.standard.string(forKey: "UNIT") ?? "")
+        // root
+        NavigationStack {
+            // main container
+            ZStack {
+                // background color
+                Color.main.ignoresSafeArea()
+                // inner container
+                VStack {
+                    TopBar()
+                    
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            }
+        }
     }
 }
 
 #Preview {
     HomeView()
 }
+
+@ViewBuilder
+func TopBar() -> some View {
+    
+    var color: Color = .main
+    
+    HStack(spacing: 10) {
+        // Date
+        Text(Date.now, format: .dateTime.day().month())
+            .font(.title2)
+            .fontWeight(.bold)
+            .foregroundStyle(color)
+        
+        Spacer()
+        
+        // Charts
+        Button {
+            // todo
+        } label: {
+            Image(systemName: "chart.bar.fill")
+                .font(.title2)
+                .foregroundStyle(color)
+        }
+        
+        // Settings
+        Button {
+            // todo
+        } label: {
+            Image(systemName: "gearshape.fill")
+                .font(.title2)
+                .foregroundStyle(color)
+        }
+    }
+    .frame(maxWidth: .infinity)
+    .padding(.vertical)
+    .padding(.horizontal, 25)
+    .background {
+        Color.white.clipShape(RoundedRectangle(cornerRadius: 22)).ignoresSafeArea().shadow(radius: 5)
+    }
+}
+
